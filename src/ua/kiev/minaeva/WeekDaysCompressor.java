fa931519c002b0483521caea9dde5ddacfb12545
minaeva <sminaeva@uscold.com>
2020-06-06 10:11:24 +0300

    Initial commit

---
+++ src/ua/kiev/minaeva/WeekDaysCompressor.java
@@ -0,0 +1,71 @@
+package ua.kiev.minaeva;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class WeekDaysCompressor {
+
+    private static String input;
+    private static String result;
+
+    public static void main(String[] args) {
+        sortAndUnifyInput();
+        compressResult();
+        printResult();
+    }
+
+    private static void sortAndUnifyInput() {
+        Scanner in = new Scanner(System.in);
+        String rawInput = in.next();
+
+        if (rawInput.length() == 0) {
+            throw new RuntimeException("Empty line");
+        }
+
+        input = rawInput.chars()
+                .mapToObj(c -> (char) c)
+                .sorted()
+                .distinct()
+                .map(c -> String.valueOf(c))
+                .collect(Collectors.joining(""));
+
+        if (input.charAt(input.length() - 1) > 7 || input.charAt(0) < 1) {
+            throw new RuntimeException("Numbers should be [1-7]");
+        }
+    }
+
+    private static void compressResult() {
+        result = String.valueOf(input.charAt(0));
+
+        for (int currentStep = 1; currentStep < input.length(); currentStep++) {
+            if (isConsequent(currentStep)) {
+                result += "-" + input.charAt(currentStep);
+                if ((currentStep > 1) && (isSecondConsequent(currentStep))) {
+                    result = result.substring(0, result.length() - 3) + input.charAt(currentStep);
+                }
+            } else {
+                result += "," + input.charAt(currentStep);
+            }
+        }
+    }
+
+    private static boolean isConsequent(int i) {
+        if (Integer.valueOf(input.charAt(i)) - Integer.valueOf(input.charAt(i - 1)) == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isSecondConsequent(int i) {
+        if (Integer.valueOf(input.charAt(i - 1)) - Integer.valueOf(input.charAt(i - 2)) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void printResult() {
+        System.out.println(result);
+    }
+
+}
